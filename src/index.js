6cc18d4efc95a6b9b7d33e67f06eda87907a01a3
same2566 <archdude4458@gmail.com>
2020-02-16 10:20:51 +0530

    static listing added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
+import "./css/algolia.css";
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
