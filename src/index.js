8fdefe75d61b2c544e60d64ed0fecc109a42e7e8
same2566 <archdude4458@gmail.com>
2020-02-16 12:54:24 +0530

    static theme listing added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,5 @@ import "./css/main.css";
 import "./css/algolia.css";
 
 
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
